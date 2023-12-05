87026165e5a79a2633075afe504207ad88147b82
Denis Chernyatin <ya.chernyatin@gmail.com>
2023-12-05 17:45:36 +0300

    Initial Commit

---
+++ CalculatorSwiftUI/CalculatorSwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  CalculatorSwiftUIApp.swift
+//  CalculatorSwiftUI
+//
+//  Created by Denis Chernyatin on 2023.12.05.
+//
+
+import SwiftUI
+
+@main
+struct CalculatorSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
