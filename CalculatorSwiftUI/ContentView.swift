baba0d63e671ed87deee480dbfce6a6a39849e3e
Denis Chernyatin <ya.chernyatin@gmail.com>
2023-12-05 20:34:03 +0300

    Design Aproved

--- CalculatorSwiftUI/ContentView.swift
+++ CalculatorSwiftUI/ContentView.swift
@@ -8,13 +8,65 @@
 
 import SwiftUI
 
+enum CalculatorButton: String {
+    
+    case zero, one, two, three, four, five, six, seven, eight, nine, dot
+    case equals, plus, minus, multiply, devide
+    case ac, plusMinus, percent
+    
+    var title: String {
+        switch self {
+        case .zero: return "0"
+        case .one: return "1"
+        case .two: return "2"
+        case .three: return "3"
+        case .four: return "4"
+        case .five: return "5"
+        case .six: return "6"
+        case .seven: return "7"
+        case .eight: return "8"
+        case .nine: return "9"
+        case .dot: return "."
+        case .devide: return "/"
+        case .multiply: return "X"
+        case .minus: return "-"
+        case .plus: return "+"
+        case .equals: return "="
+        case .plusMinus: return "+/-"
+        case .percent: return "%"
+        default:
+            return "AC"
+        }
+    }
+    
+    var backgroundColor: Color {
+        switch self {
+        case .zero, .one, .two, .three, .four, .five, .six, .seven, .eight, .nine, .dot:
+            return Color(.darkGray)
+        case .ac, .plusMinus, .percent:
+            return Color(.lightGray)
+        default:
+            return Color(.orange)
+        }
+    }
+}
+
+class GlobalEnvironment: ObservableObject {
+    
+    @Published var display = "00"
+    
+}
+
 struct ContentView: View {
     
-    let buttons = [
-        ["7","8","9","X"],
-        ["4","5","6","-"],
-        ["1","2","3","+"],
-        ["0",".",".","="]
+    @EnvironmentObject var env: GlobalEnvironment
+    
+    let buttons: [[CalculatorButton]] = [
+        [.ac, .plusMinus, .percent, .devide],
+        [.seven, .eight, .nine, .multiply],
+        [.four, .five, .six, .minus],
+        [.one, .two, .three, .plus],
+        [.zero, .dot, .equals]
     ]
     
     var body: some View {
@@ -25,19 +77,24 @@ struct ContentView: View {
             VStack(spacing: 12) {
                 HStack {
                     Spacer()
-                    Text("42").foregroundColor(.white)
+                    Text(env.display).foregroundColor(.white)
                         .font(.system(size: 64))
                 }.padding()
                 
                 ForEach(buttons, id: \.self) { row in
                     HStack (spacing: 12){
                         ForEach(row, id: \.self) { button in
-                            Text(button)
-                                .font(.system(size: 32))
-                                .frame(width: self.buttonWidth(), height: self.buttonWidth())
-                                .foregroundColor(.white)
-                                .background(Color.yellow)
-                                .cornerRadius(self.buttonWidth())
+                            
+                            Button {
+                                
+                            } label: {
+                                Text(button.title)
+                                    .font(.system(size: 32))
+                                    .frame(width: self.buttonWidth(button: button), height: (UIScreen.main.bounds.width - 5 * 12) / 4)
+                                    .foregroundColor(.white)
+                                    .background(button.backgroundColor)
+                                    .cornerRadius(self.buttonWidth(button: button))
+                            }
                         }
                     }
                 }
@@ -45,13 +102,17 @@ struct ContentView: View {
         }
     }
     
-    func buttonWidth() -> CGFloat {
+    func buttonWidth(button: CalculatorButton) -> CGFloat {
+        if button == .zero {
+            return (UIScreen.main.bounds.width - 4 * 12) / 4 * 2
+        }
+        
         return (UIScreen.main.bounds.width - 5 * 12) / 4
     }
 }
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
-        ContentView()
+        ContentView().environmentObject(GlobalEnvironment())
     }
 }
